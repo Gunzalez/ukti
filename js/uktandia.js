b46646fb74cc9d30a9d0477a92b525c2bcfca06a
Segun Konibire <segun.konibire@springstudios.com>
2014-03-08 20:44:57 +0000

    Refactors the JavaScript

--- js/uktandia.js
+++ js/uktandia.js
@@ -9,45 +9,72 @@ Date: 08.03.2014
 	
 	uktandia.carousel = {
 		
-		isBusy: false,
+		/* properties */		
+		timer: '',
+		delay: 7000,
 		$html : $('#carousel'),
 		$slides : $('.slides', this.$html),
-		$buttons: $('.controls a', this.$html),
+		$buttons: $('.controls a', this.$html),		
+		
+		/* methods */		
+		_goToSlideForThisButton: function(button){
+			
+			var self = this;
+			
+			// change button states
+			self.$buttons.removeClass('active');
+			$(button).addClass('active');
+			
+			// adjust background css
+			var index = self.$buttons.index($(button));
+			self.$html.removeAttr('class');
+			self.$html.addClass('bg-pos-'+index);
+			
+			// adjust slides css
+			var newTop = $(button).attr('data-slide-pos');
+			self.$slides.css('margin-top', newTop+'px');
+		
+		},
 		
 		init: function(){
 			
 			var self = this; // scope			
 			
+			// sets up button clicks
 			self.$buttons.on('click', function(e){
 				
 				e.preventDefault(); // prevent default action
 				
-				if(!self.isBusy){		// check if busy
-				
-					if(!$(this).hasClass('active')){	// check if already active
-						
-						// change button states
-						self.$buttons.removeClass('active');
-						$(this).addClass('active');
-						
-						// adjust background css
-						var index = self.$buttons.index($(this));
-						self.$html.removeAttr('class');
-						self.$html.addClass('bg-pos-'+index);
-						
-						// adjust slides css
-						var newTop = $(this).attr('data-slide-pos');
-						self.$slides.css('margin-top', newTop+'px');
-						
-					}
+				if(!$(this).hasClass('active')){	// check if already active
+					
+					self._goToSlideForThisButton(this);
+					
+					// disable auto
+					clearTimeout(self.timer);					
 				}
-			});
-			
+				
+			});		
+		
+			// sets up auto
+			self.timer = setInterval(function(){
+										
+					var curIndex = self.$buttons.index($('.active'));
+										
+					curIndex++;
+					var newIndex = curIndex > 2 ? 0 : curIndex;
+					
+					self._goToSlideForThisButton(self.$buttons.eq(newIndex));
+					
+			}, self.delay );
+					
 		}
-	} // carousel ends */	
+		
+	} /* carousel end */	
 	
 	$( document ).ready(function() {
+		
 		uktandia.carousel.init();	// initiate carousel
+		
 	});
 
 })(jQuery);
